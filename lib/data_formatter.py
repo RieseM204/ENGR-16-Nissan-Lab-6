c9a05be8a4f408fc07cd743f7ecbbe798658bc6f
Lily <riese.marshall204@gmail.com>
2024-11-18 00:23:03 -0600

    fixed data

--- lib/data_formatter.py
+++ lib/data_formatter.py
@@ -7,24 +7,6 @@ import numpy as np
 import pandas as pd
 from scipy.signal import savgol_filter
 
-#Section 1: reading from csvs to df and ndarray
-
-mat_list = ['rubber', 'wood']
-f_type_list = ['static', 'kinetic']
-
-color_list = ['darkorange', 'lightorange', 'hotpink', 'green', 'yellowneon']
-
-def trial_to_df(material : str, f_type : str, n : int) -> pd.DataFrame:
-    """Takes terms and int as input and returns the corresponding data"""
-    df = pd.read_csv(f"bin/friction_{material}_{f_type}_{n}.csv")
-    df = df.dropna(axis='index', how='any')
-    return df
-
-def trialdf_to_nda(df : pd.DataFrame, color : str) -> np.ndarray(shape=(2,)):
-    """Takes a dataframe from a trial and isolates the position of a single dot into an array"""
-    pos_dat = np.array([df[f'position_px_x-{color}'], df[f'position_px_y-{color}']])
-    return pos_dat
-
 #Section 2: Stacking
 
 def a_stack(a : np.ndarray(shape=(1,)), b : np.ndarray(shape=(1,))) -> np.ndarray(shape=(2,)):
@@ -103,23 +85,6 @@ def a_3d_recombine(A, axis = 2):
 
     else:
         raise ValueError(f"Axis {axis} is out of bounds for a 3D array.")
-    
-#Section 4: putting the data together into a 3d array
-
-def trial_to_3d(material : str, f_type : str, n : int, full : bool):
-    """Takes in parameters for a given trial and returns a 3d matrix of the positions"""
-    df = trial_to_df(material, f_type, n)
-    stacks_2d = []
-    if full:
-        for color in color_list:
-            stacks_2d.append(np.array(trialdf_to_nda(df, color)))
-        out = a_3d_stack(stacks_2d)
-        return out
-    else:
-        for color in color_list[:3]:
-            stacks_2d.append(np.array(trialdf_to_nda(df, color)))
-        out = a_3d_stack(stacks_2d)
-        return out
 
 # Other stuff
 
--- run.py
+++ run.py
@@ -7,3 +7,21 @@ import lib.data_formatter as datform                                            
 import lib.trig as trig                                                             # Import my trigonometry library
 import lib.phys as phys                                                             # Import my physics equation library
 import matplotlib.pyplot as plt
+
+colors = ["Green", "White", "Yellow"]
+
+def test():
+    for color in colors:
+        df = pd.read_csv(f"bin/{color}_Spring.csv")
+        df = df.dropna(axis = "index", how = "any")
+        pos_x = df["position_px_x-hotpink"].tolist()
+        pos_y = df["position_px_y-hotpink"].tolist()
+        ts = df["timestamp"].tolist()
+        print(f"{color} pos_x: {pos_x}")
+        print(f"{color} pos_y: {pos_y}")
+        print(f"{color} timestamp: {ts}")
+    raise NotImplementedError
+
+if __name__ == "__main__":                                                        # Boilerplate to show that this is the main file
+    test()
+    pass
